c9819c7b8d996d108f166b3e61724bc6e728c37b
D3ENNY <denysraimondi06@gmail.com>
2023-10-31 15:09:58 +0100

    implements equalsIgnoreCase func

--- TextBox.cs
+++ TextBox.cs
@@ -19,7 +19,13 @@ public static class StringBox
             : str == ""
             ? ErrorManager.PrintException("la stringa in input è vuota", new ArgumentException(nameof(str) + " can not be white"))
             : string.Concat(str[0].ToString().ToUpper(), str.AsSpan(1));
-
+    /// <summary>
+    /// controlla se una stringa è uguale all'altra in modo non case sensitive
+    /// </summary>
+    /// <param name="str">La stringa da capitalizzare.</param>
+    /// <returns>
+    /// true se il condronto corrispondde, viceversa false
+    /// </returns>
     public static bool EqualsIgnoreCase(string str, string value)
     {
         if(str == null)
